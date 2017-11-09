a07bc2b714274777157aea8389b1fe628b66c3c6
Tyler McDonald <tylermcdonald@Tylers-MacBook-Pro.local>
2017-11-09 14:16:49 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,28 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+	},
+
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+	},
+
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png",
+	}
+];
 var cardsInPlay = [];
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) { 
@@ -9,11 +33,13 @@ var checkForMatch = function () {
 	}
 };
 var flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
 		checkForMatch ();
 		}
-	console.log ("User flipped " + cards[cardId]);
+	console.log ("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	
 };	
 
@@ -22,3 +48,7 @@ flipCard (2);
 
 console.log(cardsInPlay[0])
 console.log(cardsInPlay[1])
+
+
+
+
